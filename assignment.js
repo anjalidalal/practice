5419123c9c939e995cf320e7dff4ed7c377fb3fe
anjalidalal <anjalimaria2002@gmail.com>
2021-10-10 13:48:36 +0530

    js practice

---
+++ assignment.js
@@ -0,0 +1,875 @@
+// /*
+// console.log("Hello Massai");
+// console.log("Massai School");
+// console.log("A TRANSFORMATION IN EDUCATION!!");
+
+// var nameOf;
+// nameOf = "Anjali Dalal";
+// console.log(nameOf);
+
+// var age = 19;
+// console.log(age);
+
+// var loc = "Karor Road, IMT, Kheri sadh";
+// var city = "Rohtak";
+// var state = "Haryana";
+// console.log('Location - ' + loc + ' | City - ' + city + ' | State - ' + state);
+
+// var gender = "female";
+// var height = 156;
+// var foodPrefrence = "PIZZA";
+// console.log(typeof gender);
+// console.log(typeof height)
+// console.log(typeof foodPrefrence)
+
+// let nameMy = "Anjali";
+// console.log(nameMy);
+// nameMy = "Ashok";
+// console.log(nameMy);
+// nameMy = "Sushila";
+// console.log(nameMy);
+
+// var side = 10;
+// var squarePerimeter = 4 * side;
+// var squareArea = side*side;
+// console.log(squareArea);
+// console.log(squarePerimeter);
+
+// var one = 5;
+// var two = 10;
+// var three = 15;
+// var product = one*two*three;
+// var sum = one+two+three;
+// console.log(sum);
+// console.log(product);
+
+// var num = 4;
+// var cube = num*num*num;
+// console.log(cube);
+
+// var big = "500";
+// var small = "300";
+// var difference = big - small;
+// var add = big + small;
+// var mulp = big * small;
+// console.log(mulp);
+// console.log(add);
+// console.log(difference);
+
+// var totalPencil = 150;
+// var disbuEqually = totalPencil/3;
+// console.log(disbuEqually);
+
+// var officeExp = 125;
+// var homeExp = 155;
+// var schlExp = 550;
+// totalExp = officeExp + homeExp + schlExp;
+// console.log(totalExp);
+
+// console.log("Massai School\nWelcome");
+
+// var birthYear = 2002;
+// var currentYear = 2021;
+// var age = currentYear - birthYear;
+// console.log(age);
+
+// var choclates = 19;
+// var pencils = 28;
+// var children = 5;
+// var remainChoclates = choclates % children;
+// var remainPencils = pencils % children;
+// console.log(remainChoclates);
+// console.log(remainPencils);
+
+// var income = 55889;
+// var expense = 22200;
+// var salary = income - expense;
+// console.log(salary);
+
+// var priceItem1 = 80;
+// var quantityItem1 = 6;
+// var amount1 = priceItem1 * quantityItem1;
+// console.log(amount1);
+
+// var priceItem2 = 140;
+// var quantityItem2 = 3;
+// var amount2 = priceItem2 * quantityItem2;
+// console.log(amount2);
+
+// var taxItem1 = amount1 * (5/100);
+// console.log(taxItem1);
+
+// var taxItem2 = amount2 * (5/100);
+// console.log(taxItem2);
+ 
+// var discountItem1 = amount1 * (8/100);
+// console.log(discountItem1);
+
+// var discountItem2 = amount2 * (8/100);
+// console.log(discountItem2);
+
+// var item1 = amount1 - discountItem1 + taxItem1;
+// console.log(item1);
+
+// var item2 = amount2 - discountItem2 + taxItem2;
+// console.log(item2);
+
+// var tip = 50;
+// var bill = item1 + item2 + tip;
+// console.log(bill);
+
+// var product1 = 100;
+// var quantity1 = 4;
+// var price1 = product1 * quantity1;
+
+// var product2 = 200;
+// var quantity2 = 5;
+// var price2 = product2 * quantity2;
+
+
+// var product3 = 300; 
+// var quantity3 = 6;
+// var price3 = product3 * quantity3;
+
+// var totalAmount = price1 + price2 + price3;
+// console.log(totalAmount); 
+
+// var marks1 = 10;
+// var marks2 = 20;
+// var marks3 = 30;
+// var marks4 = 40;
+// var marks5 = 50;
+// var marks6 = 60;
+// var average = (marks1 + marks2 + marks3 + marks4 + marks5 + marks6) / 2;
+// console.log(average);
+
+// var totalMonth = 26;
+// var remainingMonth = totalMonth % 12;
+// console.log(remainingMonth);
+
+// var side1 = 12;
+// var side2 = 7;
+// var rectPeremeter = 2*(side1 + side2);
+// var rectArea = side1 * side2;
+// console.log(rectPeremeter);
+// console.log(rectArea);
+
+// var a = 15;
+// var b = 20;
+// if (a > b) {
+//     console.log("a is big");
+// }
+// else{
+//     console.log("a is small");
+// }
+
+// var paswwd1 = "ABCD";
+// var paswwd2 = "abcd";
+// if (paswwd1 == paswwd2) {
+//     console.log("Right Password");
+// } 
+// else{
+//     console.log("Wrong Password");
+// }
+
+// var number = 29;
+// if (number % 2 == 0) {
+//     console.log("Even Number");
+// }
+// else{
+//     console.log("Odd Number")
+// }
+
+// var X = 5;
+// var Y = 10;
+// (X > Y) ? console.log("A is greater") : console.log("B is greater");
+// (X == Y) ? console.log("EQUAL") : console.log("NOT EQUAL");
+
+// /*simple interest*/
+// var P = 10000;
+// var I = 8/100;
+// var T = 2;
+// var simpleInterest = (P * I * T) / 100; 
+// console.log(simpleInterest);
+
+// /* shirt sizes */
+// var size = "XS"
+// switch (size) {
+//     case "XS":
+//         console.log("EXTRA SMALL");
+//         break;
+
+//     case "S":
+//         console.log("SMALL");
+//         break;
+
+//     case "M":
+//         console.log("MEDIUM");
+//         break;
+
+//     case "L":
+//         console.log("LARGE");
+//         break;
+
+//     case "XL":
+//         console.log("EXTRA LARGE");
+//         break;
+//     default:
+//         break;
+// }
+
+// /* COMPARE NUMBERS */
+// var A = 40;
+// var B = 20;
+// if (A > B) {
+//     console.log("A > B")
+// } else if (A < B) {
+//     console.log("A < B")
+// }
+// else if (A == B) {
+//     console.log("A == B")
+// }
+
+// /* EVEN ODD */
+// var P = 88;
+// (P % 2 == 0) ? console.log("EVEN NUMBER") : console.log("ODD NUMBER");   
+  
+// /* CUNTRY CONTINENT */
+// var country = "Argentina";
+// switch (country) {
+//     case "India":
+//         console.log("ASIA");
+//         break;
+
+//     case "Sweden":
+//         console.log("EUROPE");
+//         break;
+
+//     case "Singapore":
+//         console.log("ASIA");
+//         break;
+    
+//     case "Canada":
+//         console.log("NORTH AMERICA");
+//     break;
+
+//     case "Brazil":
+//         console.log("SOUTH AMERICA");
+//         break;
+
+//     case "Argentina":
+//         console.log("SOUTH AMERICA");
+//         break;
+
+//     case "Mexico":
+//         console.log("NORTH AMERICA");
+//         break;
+
+//     case "China":
+//         console.log("ASIA");
+//         break;
+
+//     case "Nepal":
+//         console.log("ASIA");
+//         break;
+
+//     case "Germany":
+//         console.log("EUROPE");
+//         break;
+
+//     case "France":
+//         console.log("EUROPE");
+//         break;
+
+//     case "South Africa":
+//         console.log("AFRICA");
+//         break;
+
+//     case "Nigeria":
+//         console.log("AFRICA");
+//         break;
+
+//     case "USA":
+//         console.log("NORTH AMERICA");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// /* BMI */
+// var weight = 1500;
+// var height = 50;
+// var BMI = weight/height;
+// switch (BMI) {
+//     case 18.5:
+//         console.log("You're in the underweight range")
+//         break;
+
+//     case 18.5:
+//     case 24.9:
+//         console.log("You're in the healthy weight range")
+//         break;
+
+//     case 25:
+//     case 29.9:
+//         console.log("You're in the overweight range")
+//         break;
+    
+//     case 30:
+//     case 39.9:
+//         console.log("You're in the obese range")
+//         break;
+
+//     default:
+//         break;
+// }
+
+// /* COUPON DISCOUNT CALCULATOR */
+// var purchase = 300;
+// var discount = purchase * 10/100
+// if (purchase <= 300) {
+//     if (discount <= 100) {
+//         console.log("Maximum Discount");
+//     } else {
+//        console.log("Minimum discount") 
+//     }
+// }
+
+// /* DRIVE AGE CHECK */
+// var age = 18
+// if (age > 18) {
+//    console.log("You can drive in India!!"); 
+// }
+// else if (age < 18) {
+//     console.log("You cannot drive in India!!");
+// }
+// else if (age == 18) {
+//     console.log("You are just old enough to drive in India!!")   
+// }    
+
+// /* INDIAN STATE REGION */
+// var states = "Kerla";
+// switch (states) {
+//     case "Punjab":
+//     case "Haryana":
+//     case "U.P":
+//     case "Rajasthan":
+//         console.log("NORTH STATES");    
+//         break;
+
+//     case "Kerla":
+//     case "Tamil Nadu":
+//     case "Andhra Pardesh":
+//     case "Karnataka":
+//         console.log("SOUTH STATES");    
+//         break;
+
+//     case "Bihar":
+//     case "Jharkhand":
+//     case "Odisha":
+//     case "West Bengal":
+//         console.log("EAST STATES");    
+//         break;
+
+//     case "Gujarat":
+//     case "Maharashtra":
+//         console.log("WEST STATES");    
+//         break;
+
+//         default:
+//         break;
+// }
+//  /* VOWEL CHECK */
+// var alpha = "c";
+// switch (alpha) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log("VOWEL");
+//         break;
+
+//     default:
+//         console.log("CONSTANT");
+//         break;
+// }
+
+// /* VOTING RIGHTS */
+// var vote = 56;
+// if (vote >= 18) {
+//     console.log("YES!!");
+// } else {
+//     console.log("NOO!!");
+// }
+
+// /* LEAP YEAR CHECK */
+// var year = 2020;
+// if (year % 4 == 0) {
+//     console.log("Leap Year");
+// } else {
+// console.log("Not Leap Year");
+// }
+
+// /* PINCODE */
+// var pincode = 23455;
+// if (pincode >= 100000 && pincode <= 999999) {
+//     console.log("VALID");
+// } else {
+//     console.log("INVALID");
+// }
+
+// /* LOOPS */
+// var i = 1;
+// while (i <= 5) {
+//     i++;
+//     console.log("Massai School");
+// }
+
+// /* multiples of 3 */
+
+// var limit = 30;
+// var sum = 0;
+// var average;
+// for (var i = 0; i <= limit; i++) {  
+//     if (i % 3 == 0) {
+//         sum += i;
+//         average = sum/2;
+//     }
+// } 
+//     console.log(average);
+
+//     
+// // ARRAY SUM
+// var arr = [1, 2, 3, 4, 5];
+// sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     console.log(sum);
+// }
+
+
+// LOWEST SCORE
+ //var array = [43, 65, 50, 54, 23];
+//var lowest = array[0];
+//for (let i = 0; i < array.length; i++) {
+     //if (array[i] < lowest ) {
+     //   lowest = array[i];
+     //}
+    //}
+     //console.log(lowest);
+  
+
+//PRIME CHECK
+// var i = 2;
+// var newNum = 26;
+// while(i < newNum){
+//     if (newNum % i == 0) {
+//         console.log("NOT PRIME");
+//     }
+//     else{ 
+//         console.log("PRIME");
+//     }
+//     i++;
+// }
+//  //while i--
+//  var i = 24;
+//  var j = 16;
+//  while (i > j) {
+//      console.log(i);
+//      i--;
+//  }
+
+// //PRINT PATTREN
+// for (let i = 1; i < 10; i++) {
+//     for (let j = 1; j < 10; j++) {
+//         for (let k = 1; k < 10; k++) {
+//             console.log(i + "." + j + "." + k);
+//         }    
+//     }
+// }
+// // NUMBER SPLIT
+// var str = "59485";
+// for (let i = 0; i < str.length; i++) {
+//     if (i == str[i]) {
+//         console.log(str[i]);
+//     }
+//     console.log(str[i] * 10);
+// }
+
+// //PALINDROME
+// let str = "nitin";
+// // var revStr;
+// for (let i = str.length - 1; i <= str[0]; i--) {
+//     //revStr = orgnlStr[i]
+//     console.log(str);
+// }
+
+// let exp = 1;
+// for (let i = 0; i < 4; i++) {
+//     exp *= 2
+// }
+// console.log(exp)
+
+
+
+//CONSONANT CHECK
+// let arr = ["anju", "mnju", "snju"];
+// let output = 0;
+// for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i] !== "a" || arr[i] !== "e" || arr[i] !== "i" || arr[i] !== "o"
+//     || arr[i] !== "u" ) {
+//         output++;
+//     }  
+// }
+// console.log(output);
+
+
+//COUNT EVEEN ODD SEPREATELY
+// let ending = 20;
+// let evenSum = 0;
+// let oddSum = 0;
+// let oddAverage;
+// let evenAverage;
+// for (let i = 0; i <= ending; i++) {
+//     if (i % 2 == 0) {
+//         evenSum += i;
+//         evenAverage = evenSum/2;
+//     }
+//     else{ 
+//         oddSum += i;
+//         oddAverage = oddSum/2;
+//     }
+// }
+// console.log(evenAverage);
+// console.log(oddAverage);
+
+
+//PRIME OR NOT
+/*let num = 5;
+let i = 2;
+let isPrime = true;
+while (i < 5) {
+    if (num % i == 0) {
+        isPrime = false;
+    }
+    i++;
+}
+if (isPrime) {
+    console.log("prime");
+}
+else{
+    console.log("not prime");
+}*/
+
+
+// //convert 3987 into three nine eight seven 
+// let n = "3987";
+// let length = n.length;
+// let output = " ";
+// for (let i = 0; i <= length ; i++) {
+//     //console.log(i);
+//    var digit = Number(n[i]);
+//     switch (digit) {
+//         case 0:
+//             output += " Zero ";
+//             break;
+//         case 1: 
+//             output += " One ";
+//             break;
+//         case 2:
+//             output += " Two ";
+//             break;
+//         case 3:
+//             output += " Three ";
+//             break;
+        // case 4:
+        //     output += " Four ";
+        //     break;
+        // case 5:
+        //     output += " Five ";
+        //     break;
+        // case 6:
+        //     output += " Six ";
+        //     break;
+        // case 7:
+        //     output += " Seven ";
+        //     break;
+        // case 8:
+        //     output += " Eight ";
+        //     break;
+        // case 9:
+//             output += " Nine ";
+//             break;
+//         default:
+//             break;
+//     }  
+//  }
+//    console.log(output);
+
+
+//print odd characters
+// var str = "windows";
+// for (let i = 0; i < str.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(str[i]);
+//     } 
+// }
+
+
+//3171
+//2. Calculate exponent
+// var num = 2;
+// var expo = 4;
+// var result = 1;
+// for (let i = 1; i <= expo; i++){
+//     result *= num;
+// }
+// console.log(result);
+
+
+//prime no count
+// var limit = 45;
+// var num;
+// for (let i = 1; i < limit; i++) {
+//     var isPrime = true;
+//     //console.log(i);
+//     for (let j = 2; j < i; j++) {
+//         //console.log(i + "." + j);
+//          if (i % j == 0) {
+//              isPrime = false;
+//          }   
+//     } 
+//     if (isPrime) {
+//          console.log(i);
+//      }    
+// }
+
+
+
+//calender
+// for (let i = 2020; i <= 2020; i++) {
+//     for (let j = 1; j <= 12; j++) {
+//     for (let k = 31; k <= 30 ; k--) {
+//         console.log(i + "-" + j + "-" + k);
+        
+//       }    
+//     }  
+// }
+
+
+
+//PALINDROME
+// var arr = ['N', 'i', 't', 'i', 'n'];
+// var rev = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     //console.log(arr[i]);
+//     rev.push(arr[i]);
+    
+// }
+// console.log(rev);
+// var isPalindrome = true;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== rev[i]) {
+//         isPalindrome = false;
+//     }  
+// }
+// if (isPalindrome) {
+//     console.log("YES");
+// }
+// else{
+//     console.log("NO");
+// }
+
+
+//count words
+// let str = "Their is a smallest natural number";
+// let reslt = 1;
+// for (let i = 1; i < str.length; i++) {
+//     //console.log(i)
+//     if (str[i] === " " ){
+//        reslt++; 
+//     }
+    
+// }
+// console.log(reslt);
+
+
+//reverse array
+// let arr = ["A", "E", "I", "O", "U",];
+// for (let i = arr.length - 1; i >= 0 ; i--) {
+//     console.log(arr[i]);   
+// }
+
+
+//join array comma
+// let arr = ["HTML", "CSS", "JAVA", "JS"];
+// let str = "";
+// for (let i = 0; i < arr.length; i++) {
+//    if (arr[i] === " ") {
+//        str += " , "
+//    }
+    
+// }
+// console.log(str);
+
+
+//remove small words
+// let array = ["A", "Quick", "Brown", "Fox", "Over", "A", "Lazy", "Dog"];
+// let newArr = [];
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i].length >= 4) {
+//         newArr.push(array[i]);
+//     } 
+// }
+// console.log(newArr);
+
+
+//3266
+//2D ARRAY SUM
+
+// let array = [
+//     [1, 2, 3],
+//     [5, 6, 7],
+//     [7, 8, 9]
+// ];
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//         sum += array[i][j];  
+//     }    
+// }
+// console.log(sum);
+  
+        
+//2D ARRAY COLUMN SUM
+// let array = [
+//     [1, 2, 3],
+//     [5, 6, 7],
+//     [7, 8, 9]
+// ];
+// for (let j = 0; j < array.length; j++) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i][j];  
+//     }
+//     console.log(sum);  
+// }
+  
+
+//PRINT SUBMATRIX
+
+// let twoDArray = [
+//     [1, 2, 3],
+//     [5, 6, 9],
+//     [7, 8, 0]
+// ];
+// // console.log(twoDArray[0].length)
+// for (let col = 0; col < twoDArray[0].length; col++) {
+//     for (let row = 0; row < twoDArray.length; row++) {
+//         console.log({col, row})
+        
+//     }
+// }
+
+//SUB MATRIX
+// let matrix = [
+//     [1, 2, 3],
+//     [5, 6, 7],
+//     [7, 8, 9]
+// ];
+// let rs = 1;
+// let cs = 1;
+// let re = 2;
+// let ce = 2;
+// let subMatrix = [];
+// for (let i = rs; i <= re; i++){
+//     let newRow = [];
+//     for (let j = cs; j <= ce; j++) {
+//        newRow.push(matrix[i][j]);
+//        //console.log(newRow)
+//     }
+//     subMatrix.push(newRow);
+// }
+// console.log(subMatrix);
+        
+
+//MATRIX ADDITION
+let matrix1 = [
+[1, 2, 3],
+[5, 6, 7],
+[7, 8, 9]
+];
+
+let matrix2 = [
+[2, 1, 2],
+[1, 2, 1],
+[2, 1, 2]
+];
+let newMatrix = [];
+for (let n = 0; n < matrix1.length; n++) {
+    let sum = 0;
+    let newRow = [];
+    for (let m = 0; m < matrix2.length; m++) {
+        sum = matrix1[n][m] + matrix2[n][m];
+        newRow.push(sum);
+    }
+    newMatrix.push(newRow);
+}
+// console.log(newMatrix);
+
+//TRIANGLE SUM
+//let matrix = [
+    // [1, 2, 1],
+    // [2, 1, 2],
+    // [1, 2, 1]
+//     [1, 2, 3],
+//     [5, 6, 7],
+//     [7, 8, 9]
+// ];
+// let triangleSum = 0;
+// for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix.length; j++) {
+//        if (matrix[i][1] == matrix[j][0] && matrix[i][0] == matrix[j][1] && matrix[i][1] == matrix[j][2] && matrix[i][2] == matrix[j][1]) {
+//            triangleSum += matrix[i][j];
+//        }    
+//     }
+// }
+// console.log(triangleSum);
+
+// let matrix = [
+//     [1, 2, 3],
+//     [5, 6, 7],
+//     [7, 8, 9]
+// ];
+// let triangleSum = 0;
+// for (let row = 0; row < matrix.length; row++) {
+//     for (let col = row; col < matrix.length; col++) {
+//        triangleSum += matrix[row][col];    
+//     }
+// }
+// console.log(triangleSum);
+
+// //DIAGONAL DIFFERENCE
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [0, 8, 9]
+];
+let difference;
+let sum1 = 0;
+let sum2= 0;
+
+for (let i = 0; i < matrix.length; i++) {
+   for (let j = 0; j < matrix[i].length; j++) {
+       if (i === j) {
+           sum1 += matrix[i][j];
+       }
+       if (i + j == 2) {
+           sum2 += matrix[i][j];
+       }
+    }   
+}
+difference = sum2 - sum1;
+console.log({difference})
