a1101af795b070d5ae9a2efb39597ec9d80017b8
anjalidalal <anjalimaria2002@gmail.com>
2021-10-14 17:09:25 +0530

    details

---
+++ object/practice7.js
@@ -0,0 +1,13 @@
+let details = {
+    mummy : 946633,
+    mansi : 782451,
+    papaa : 813058,
+    anjali : 816866,
+}
+let user = "anjal";
+if (details[user]) {
+    console.log(details[user]);
+}
+else{
+    console.log("user not found");
+}
