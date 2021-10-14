b054254c0f9633671c94178d42431e594ba07e4a
anjalidalal <anjalimaria2002@gmail.com>
2021-10-14 13:53:36 +0530

    marks

--- object/practice2.js
+++ object/practice2.js
@@ -1,8 +1,39 @@
-var user = {
-    name : "Jason",
-    birth : 2002,
-    age : function(year){
-        console.log(year - this.birth)
-    } 
+var item = {
+    data: [
+        {marks: 20, name: "anjali"},
+        {marks: 30, name: "sheetal"},
+        {marks: 10, name: "mansi"}
+    ],
+    average : function(){
+        let sum = 0;
+        for (let i = 0; i < this.data.length; i++) {
+            sum += this.data[i].marks;
+        }
+        console.log("AVERAGE IS " + (sum/2))
+    },
+    maximum : function(){
+        let max = 0;
+        let name = "";
+        for (let i = 0; i < this.data.length; i++) {
+            if (max < this.data[i].marks) {
+                max = this.data[i].marks;
+                name = this.data[i].name;
+            }
+        }
+        console.log({max, name})
+    },
+    minimum : function(){
+        let min = Infinity;
+        let name ="";
+        for (let i = 0; i < this.data.length; i++) {
+            if (min > this.data[i].marks) {
+                min = this.data[i].marks;
+                name = this.data[i].name;
+            }
+        } 
+        console.log({min, name});
+    }
 }
-user.age(2022)
+item.average();
+item.maximum();
+item.minimum();
