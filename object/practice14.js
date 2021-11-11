e45b5406e3f78de30b5062d416e05a7349565233
anjalidalal <anjalimaria2002@gmail.com>
2021-11-11 12:24:33 +0530

    sum of natural numbers with boilerplate

--- object/practice14.js
+++ object/practice14.js
@@ -1,6 +1,32 @@
-let num = 3;
-let sum = 0; 
-for (let i = 0; i <= num; i++) {
-     sum += i; 
+let sum = function(num){
+     let s = 0;
+     for (let i = 0; i <= num; i++) {
+          s += i; 
+     }
+     return s;
+} 
+const runProgram = (input) => {
+     const num = (input.trim().split(" ").map(Number));
+     console.log(sum(num));
 }
-console.log(sum);
+
+if (process.env.USERNAME === "mansi") {
+    runProgram(`3`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
