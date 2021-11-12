3d919e4158c461dc27452f655f7235c3808ea1a0
anjalidalal <anjalimaria2002@gmail.com>
2021-11-12 22:20:25 +0530

    cube of number

---
+++ object/cube.js
@@ -0,0 +1,26 @@
+const runProgram = (input) => {
+    const num = (input.trim().split(" ").map(Number));
+    console.log(num **3);
+}
+
+if (process.env.USERNAME === "mansi") {
+     runProgram(`2`);
+} else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+         read += input;
+     });
+     process.stdin.on("end", function () {
+         read = read.replace(/\n$/, "");
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+     });
+     process.on("SIGINT", function () {
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+         process.exit(0);
+     });
+ }
+ 
