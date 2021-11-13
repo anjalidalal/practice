9968b14456cdc1532757ca68afd9c352c94f0fdc
anjalidalal <anjalimaria2002@gmail.com>
2021-11-13 13:54:25 +0530

    else if condition

---
+++ object/sum_conditions.js
@@ -0,0 +1,39 @@
+//Enter code here 
+ const runProgram = (input) => {
+      let lines = input.trim().split("\n");
+
+      const arr = lines[1].trim().split(" ").map(Number);
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+          sum += arr[i];   
+      }
+      if (sum > 100) {
+          console.log("Greater")
+      }
+      else{
+          console.log("Lesser")
+      }
+ }
+ 
+ if (process.env.USERNAME === "mansi") {
+     runProgram(`5
+     21 24 2 54 8`);
+ } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+         read += input;
+     });
+     process.stdin.on("end", function () {
+         read = read.replace(/\n$/, "");
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+     });
+     process.on("SIGINT", function () {
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+         process.exit(0);
+     });
+ }
+ 
