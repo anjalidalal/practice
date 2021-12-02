57e0111b9c0e6683d2b84e4708d74ca441248f23
anjalidalal <anjalimaria2002@gmail.com>
2021-12-02 23:35:31 +0530

    for loop and functions

--- object/practice9.js
+++ object/practice9.js
@@ -4,21 +4,15 @@ let name = ["Rice", "Dal", "Salt"];
 let quantity = [2, 3, 1];
 let price = [60, 50, 20];
 
-// let users = {
-//     total : function(){
-//         let product = this.quantity * this.price;
-//         return product;
-//     }
-// };
 
 let user = {
     data :[],
     total : function(){
         let sum = 0;
         for (let i = 0; i < this.data.length; i++) {
-            sum += this.data[quantity] * this.data[price];
+            sum += this.data[i].quantity * this.data[i].price;
         }
-        return sum;
+        return sum ;
     }
 }
 
@@ -32,5 +26,5 @@ for (let i = 0; i < name.length; i++) {
     user.data.push(u)
 }
 
-console.log(user, user.total())
-//users.user
+console.log(user);
+console.log("TOTAL ~ ", user.total());
