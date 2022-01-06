1074d4c9b2e15bae8a5566e771dae4f489d13c91
anjalidalal <anjalimaria2002@gmail.com>
2022-01-06 14:18:47 +0530

    Rotation of 2D array in 90 degree

--- object/rotate.js
+++ object/rotate.js
@@ -1,17 +1,44 @@
-let r = 4;
-let c = 4;
-let arr = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [1, 2, 3, 4],
-    [5, 6, 7, 8]
-];
-let rotation = [];
-for (let i = c-1; i >= 0; i--){
-    let newRow = [];
-    for (let j = 0; j < arr[0].length; j++) {
-        newRow.push(arr[j][i])
+function rotated_arr(newArr) {
+    for (let i = newArr.length - 1; i >= 0; i--) {
+        let newRow = [];
+        for (let j = 0; j < newArr[0].length; j++) {
+            newRow.push(newArr[j][i])
+        }
+        console.log(newRow.join(" "))
     }
-    rotation.push(newRow);
 }
-console.log(rotation);   
+const runProgram = (input) => {
+    let lines = input.trim().split("\n");
+    let newArr = [];
+    for (let i = 1; i < lines.length; i++) {
+        let arr = lines[i].trim().split(" ").map(Number);
+        newArr.push(arr);
+    }
+    rotated_arr(newArr)
+}
+
+if (process.env.USERNAME === "mansi") {
+    runProgram(`4
+    1 2 3 4
+    5 6 7 8
+    1 2 3 4
+    5 6 7 8`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
+
