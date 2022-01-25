1e1285b885523bd60598b321359bb9416c2331c4
anjalidalal <anjalimaria2002@gmail.com>
2022-01-26 00:54:44 +0530

    A defanged IP address replaces every period (.) with [.]

---
+++ leetcode/ipAddress.js
@@ -0,0 +1,30 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+ 
+var defangIPaddr = function(str) {
+    let arr ="";
+for (let i = 0; i < str.length; i++) {
+   if (str[i] === ".") {
+       arr += "[" + "." + "]"
+   }
+   else{
+       arr += str[i]
+   }
+}
+    return arr;
+};
+
+let str = "1.1.1.1";
+console.log(defangIPaddr(str))
