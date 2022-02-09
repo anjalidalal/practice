cd3769c851867b92a9e7b96d080fbe1a3ebf1c99
anjalidalal <anjalimaria2002@gmail.com>
2022-02-09 22:36:48 +0530

    Given an integer num, repeatedly add all its digits until the result has only one digit

---
+++ leetcode/adddigits.js
@@ -0,0 +1,50 @@
+// 258. Add Digits
+// Easy
+
+// 2252
+
+// 1615
+
+// Add to List
+
+// Share
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+// Example 2:
+
+// Input: num = 0
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= num <= 231 - 1
+ 
+
+// Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+
+var addDigits = function(num) {
+    num = String(num);
+    while (num.length > 1) {
+        let sum = 0
+        for (let i = 0; i < num.length; i++) {
+            sum += Number(num[i]);
+        }
+        num = String(sum);
+    }   
+    return num;
+};
+let num = 38;
+
+console.log(addDigits(num))
