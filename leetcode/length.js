d68167207b22196b84243c7df2bf5109d24ff076
anjalidalal <anjalimaria2002@gmail.com>
2022-03-07 22:45:16 +0530

    Capitalize the title

---
+++ leetcode/length.js
@@ -0,0 +1,13 @@
+let title = "capiTalIze tHe titLe";
+let s = title.split(" ")
+console.log(s)
+let str = ""
+var capitalizeTitle = function(title) {
+    for (let i = 0; i < s.length; i++) {
+  
+        return s[i].charAt(0).toUpperCase()+s[i]
+        
+     }
+};
+
+console.log(str)
